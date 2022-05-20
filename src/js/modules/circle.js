79feb462f6d701d0b52f609ce6dbbef2e22efeca
Musya <abashka1994@gmail.com>
2022-05-20 22:32:48 +0300

    add

--- src/js/modules/circle.js
+++ src/js/modules/circle.js
@@ -1,10 +1,11 @@
 function startCircle(countC, header, section, sectionNext) {
   let val = 0,
-  duration = 30,
+  duration = 100,
   sectionNumber = Number(countC),
   thisSection = document.querySelector('section[data-circle="'+ String(sectionNumber) +'"]'),
   thisSectionNext = document.querySelector('section[data-remove="'+ String(Number(thisSection.dataset.remove) + 1) +'"]'),
   thisModal = document.querySelector('.modal__wrapper.modal-'+countC),
+  thisText = thisSection.querySelector('.third-screen__title'),
   svg = document.querySelector('#svg' + countC),
   circle = svg.querySelector('#bar' + countC),
   count = thisSection.querySelector('.count');
@@ -14,9 +15,14 @@ function startCircle(countC, header, section, sectionNext) {
   function stop() {
     clearTimeout(startInterval);
   }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  let progressRandom=[0,6,1,3,2,5,0,2,1]
 
   function start() {
-    val++
+    val+=progressRandom[getRandomInt(progressRandom.length)];
     if(isNaN(val)) {
       val = 0;
     } else {
@@ -27,7 +33,7 @@ function startCircle(countC, header, section, sectionNext) {
       if (val > 100) { val = 100;}
       if (val == 50) {
         stop();
-        showModal(thisModal);
+        showModal(thisModal, thisText, sectionNumber);
       };
       var pct = ((100-val)/100)*c;
       circle.style.strokeDashoffset = pct;
